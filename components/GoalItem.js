fc8fdb01a7842d875e24c6a4e5338b9440658104
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-06-22 07:05:49 -0500

    This is the final Goal List App built with react native.

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,23 +1,28 @@
-import { StyleSheet, View, Text } from 'react-native'
-function GoalItem(props) {
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
+function GoalItem(props) {
   return (
     <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
+      <Pressable
+        android_ripple={{ color: '#d2c7c7' }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
 
-export default GoalItem
+export default GoalItem;
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
-    borderRadius: 30,
-    backgroundColor: '#5e0acc',
+    borderRadius: 16,
+    backgroundColor: '#55209a',
   },
   goalText: {
     color: 'white',
+    padding: 8,
   },
-})
+});
